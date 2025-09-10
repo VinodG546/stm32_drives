756271d01ccd37e8493c8b9627a740bb37839e1f
VinodG546 <gudlavinod546@gmail.com>
2025-09-10 15:33:35 +0530

    i2c updated master receive

--- bare_metal/14_i2c_driver/Inc/i2c.h
+++ bare_metal/14_i2c_driver/Inc/i2c.h
@@ -13,6 +13,6 @@
 
 void i2c1_init();
 void i2c1_master_send(uint8_t slave_addr, uint8_t *data, uint32_t size);
-
+void i2c1_master_receive(uint8_t slave_addr, uint8_t *buffer, uint32_t size);
 
 #endif /* I2C_H_ */
--- bare_metal/14_i2c_driver/Src/i2c.c
+++ bare_metal/14_i2c_driver/Src/i2c.c
@@ -94,3 +94,43 @@ void i2c1_master_send(uint8_t slave_addr, uint8_t *data, uint32_t size)
     // 7. Clear STOP flag
     I2C1->ICR |= I2C_ICR_STOPCF;
 }
+
+
+void i2c1_master_receive(uint8_t slave_addr, uint8_t *buffer, uint32_t size)
+{
+    uint32_t tmpreg;
+
+    // 1. Wait until I2C not busy
+    while (I2C1->ISR & I2C_ISR_BUSY);
+
+    // 2. Configure CR2 for slave address, number of bytes, read mode
+    tmpreg  = I2C1->CR2;
+    tmpreg &= ~(I2C_CR2_SADD | I2C_CR2_NBYTES | I2C_CR2_RD_WRN | I2C_CR2_AUTOEND);
+
+    tmpreg |= (slave_addr << 1) & I2C_CR2_SADD;   // 7-bit address
+    tmpreg |= (size << 16);                       // NBYTES
+    tmpreg |= I2C_CR2_RD_WRN;                     // Set for Read
+    tmpreg |= I2C_CR2_START;                      // Generate START
+
+    I2C1->CR2 = tmpreg;
+
+    // 3. Read incoming bytes
+    for (uint32_t i = 0; i < size; i++)
+    {
+        while (!(I2C1->ISR & I2C_ISR_RXNE));      // Wait until RXNE
+        buffer[i] = (uint8_t)I2C1->RXDR;
+    }
+
+    // 4. Wait until transfer complete
+    while (!(I2C1->ISR & I2C_ISR_TC));
+
+    // 5. Generate STOP
+    I2C1->CR2 |= I2C_CR2_STOP;
+
+    // 6. Wait until STOP flag set
+    while (!(I2C1->ISR & I2C_ISR_STOPF));
+
+    // 7. Clear STOP flag
+    I2C1->ICR |= I2C_ICR_STOPCF;
+}
+
